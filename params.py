eeceed4d438309715dd22661bc8104f6631d5b26
SahilJ97 <sahil.jayaram@gmail.com>
2018-11-16 19:45:43 -0800

    fixed toy dataset generator

--- params.py
+++ params.py
@@ -2,24 +2,29 @@ HID_LAYER_SIZE = 200
 BATCH_SIZE = 10
 BATCH_SHAPE = (BATCH_SIZE, 28*28)
 LATENT_SPACE_DIM = 50
-EPOCH_GROUPS = [10, 10, 20, 20]
-LEARNING_RATES = [.01, .005, .001, .0001]
-TRAINING_EPOCHS = sum(EPOCH_GROUPS)
 NOISE_STEP = .02
 MAX_N_LEVEL = .501
 ALPHA = .1  # standard error of the distribution from which r's entries are drawn--should be contained in [0, 1]
 BETA = 1.  # weighting coefficient for reconstruction error term.
-WRITE_EPOCHS = [0, 1, 2, 3, 6, 11, 16, 21, 26, 31, 41, 51]
 IMAGE_DIR = 'Images/'
 TRAIN_SET_SIZE = 60000
 TEST_SET_SIZE = 10000
+
 TOY = True
 
 if TOY is True:
+    EPOCH_GROUPS = [1, 10, 20]
+    LEARNING_RATES = [.01, .001, .0001]
+    WRITE_EPOCHS = [0, 1, 2, 6, 11, 26]
     LOG_FILE = 'toy_log.csv'
     TRAIN_IMAGES = 'toy_train.npy'
     TEST_IMAGES = 'toy_test.npy'
 else:
+    EPOCH_GROUPS = [10, 10, 20, 20]
+    LEARNING_RATES = [.01, .005, .001, .0001]
+    WRITE_EPOCHS = [0, 1, 2, 3, 6, 11, 16, 21, 26, 31, 41, 51]
     LOG_FILE = 'log.csv'
     TRAIN_IMAGES = 'train_images.npy'
     TEST_IMAGES = 'test_images.npy'
+
+TRAINING_EPOCHS = sum(EPOCH_GROUPS)
--- toy.py
+++ toy.py
@@ -4,27 +4,27 @@ from params import *
 
 
 if __name__ == '__main__':
-    '''
-    train = np.array([])
+
+    train = np.empty((0, 28, 28))
     # generate from this.
     for i in range(TRAIN_SET_SIZE):
-        latent = normal(0, 1, (LATENT_SPACE_DIM))
+        latent = normal(.5, .1, (LATENT_SPACE_DIM))
         image = np.zeros(28*28)
         for j in range(LATENT_SPACE_DIM):
             for k in range(10*j, 10*j + 10):
-                image[k] = latent[j]
-        train = np.append(train, np.reshape(image, (28, 28)))
+                image[k] += latent[j]  # no, need to make it a function!
+        train = np.append(train, [np.reshape(image, (28, 28))], axis=0)
     print(np.shape(train))
     np.save('toy_train', train)
-    '''
-    test = np.array([])
+
+    test = np.empty((0, 28, 28))
     # generate from this.
     for i in range(TEST_SET_SIZE):
-        latent = normal(0, 1, (LATENT_SPACE_DIM))
-        image = np.zeros(28 * 28)
+        latent = normal(.5, .1, (LATENT_SPACE_DIM))
+        image = np.zeros(28*28)
         for j in range(LATENT_SPACE_DIM):
             for k in range(10 * j, 10 * j + 10):
-                image[k] = latent[j]
-        test = np.append(test, np.reshape(image, (28, 28)))
+                image[k] += latent[j]
+        test = np.append(test, [np.reshape(image, (28, 28))], axis=0)
     print(np.shape(test))
     np.save('toy_test', test)
